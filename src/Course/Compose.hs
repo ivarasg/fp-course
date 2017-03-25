5bef5fa96031660166eab856b7cc377b8a579ab3
Ignacio Varas González <ignacio.varas.gonzalez@gmail.com>
2017-03-25 09:52:39 -0300

    solved compose exercise, styled traversable

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -10,26 +10,26 @@ import Course.Monad
 
 -- Exactly one of these exercises will not be possible to achieve. Determine which.
 
-newtype Compose f g a =
-  Compose (f (g a))
+newtype Compose f g a = Compose (f (g a))
 
 -- Implement a Functor instance for Compose
-instance (Functor f, Functor g) =>
-    Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  h <$> Compose c = Compose $ (h <$>) <$> c
+    -- error "todo: Course.Compose (<$>)#instance (Compose f g)"
 
-instance (Applicative f, Applicative g) =>
-  Applicative (Compose f g) where
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure :: a -> Compose f g a
+  pure x = Compose $ pure (pure x)
+    -- error "todo: Course.Compose pure#instance (Compose f g)"
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  Compose cf <*> Compose ca = Compose $ ((<*>) <$> cf) <*> ca
+    -- error "todo: Course.Compose (<*>)#instance (Compose f g)"
 
-instance (Monad f, Monad g) =>
-  Monad (Compose f g) where
+instance (Monad f, Monad g) => Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
+  (=<<) :: (a -> Compose f g b) -> Compose f g a -> Compose f g b
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    error "No se puede hacer!"

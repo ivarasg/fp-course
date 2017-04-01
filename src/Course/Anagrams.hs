ab5ef04c27a0c103b634ff1a7d09a0af0c748f3b
Ignacio Varas González <ignacio.varas.gonzalez@gmail.com>
2017-04-01 10:29:13 -0300

    changed lines for words

--- src/Course/Anagrams.hs
+++ src/Course/Anagrams.hs
@@ -30,11 +30,12 @@ Functions that might help
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
 anagrams :: Chars -> Filename -> IO (List Chars)
-anagrams str file = intersectBy equalIgnoringCase angrs . lines <$> readFile file
+anagrams str file = intersectBy equalIgnoringCase angrs . words <$> readFile file
    where angrs = permutations str
   -- error "todo: Course.Anagrams#anagrams"
 
 -- Compare two strings for equality, ignoring case
 equalIgnoringCase :: Chars -> Chars -> Bool
 equalIgnoringCase s1 s2 = map toLower s1 == map toLower s2
+-- equalIgnoringCase = on (==) $ map toLower
   -- error "todo: Course.Anagrams#equalIgnoringCase"

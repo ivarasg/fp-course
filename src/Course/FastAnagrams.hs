e633e52e5ff142a667236b4540051b6d0c4fd934
Ignacio Varas González <ignacio.varas.gonzalez@gmail.com>
2017-04-01 10:29:33 -0300

    solved fastAnagrams exercises

--- src/Course/FastAnagrams.hs
+++ src/Course/FastAnagrams.hs
@@ -6,21 +6,22 @@ module Course.FastAnagrams where
 import Course.Core
 import Course.List
 import Course.Functor
+import Course.Monad
+import Course.Applicative
 import qualified Data.Set as S
 
 -- Return all anagrams of the given string
 -- that appear in the given dictionary file.
-fastAnagrams ::
-  Chars
-  -> Filename
-  -> IO (List Chars)
-fastAnagrams =
-  error "todo: Course.FastAnagrams#fastAnagrams"
+fastAnagrams :: Chars -> Filename -> IO (List Chars)
+fastAnagrams str file = map ncString . intersectBy (==) angrs . map NoCaseString . words <$> readFile file
+   where angrs = NoCaseString <$> permutations str
+  -- error "todo: Course.FastAnagrams#fastAnagrams"
 
 newtype NoCaseString =
   NoCaseString {
     ncString :: Chars
   }
+  deriving Ord
 
 instance Eq NoCaseString where
   (==) = (==) `on` map toLower . ncString
